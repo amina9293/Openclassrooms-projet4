c85daf7eded3679519e06e50bfce72785c7482b8
amina9293 <61966759+amina9293@users.noreply.github.com>
2021-01-31 20:53:34 +0100

    Dernière version

--- Projet4OCR/src/com/hembiotech/analytics/ReadDocument.java
+++ Projet4OCR/src/com/hembiotech/analytics/ReadDocument.java
@@ -1,22 +1,30 @@
 package com.hembiotech.analytics;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Anything that will read symptom data from a source
+ * The important part is, the return value from the operation, which is a list of strings,
+ * that may contain many duplications
+ * 
+ * The implementation does not need to order the list
+ * @author Amina
+ *
+ * 
+ */
+
 public class ReadDocument {
-	public static void Reader(String document, ArrayList<String> listSymptoms) throws IOException {
+	public static void Reader(String document, ArrayList<String> listSymptoms) 
+															throws IOException {
 		
-		//read the symptoms file and store it in a list
-		
-			
-				//read the file
 				if (document != null) {
+					
 				BufferedReader br = new BufferedReader(new FileReader(document));
 				String line = br.readLine();
+				
 				while ((line = br.readLine()) != null){
 					
 					//see the content of the files in the console 
@@ -25,13 +33,11 @@ public class ReadDocument {
 					// add the symptoms in the list
 					listSymptoms.add(line);
 			       }
+				
 					// see the list in the console
-					
 					System.out.println(listSymptoms);
 					
 			        br.close();
-			        
-			        
 				}
 	}
 
