d3fce7ea0df1abf371adfd7f65cae1c756643b73
amina9293 <61966759+amina9293@users.noreply.github.com>
2021-02-01 12:38:26 +0100

    Update AnalyticsCounter.java

--- Projet4OCR/src/com/hembiotech/analytics/AnalyticsCounter.java
+++ Projet4OCR/src/com/hembiotech/analytics/AnalyticsCounter.java
@@ -8,6 +8,10 @@ import java.util.Map;
  * One that will read a file
  * The second that will count each Symptoms with their value and store them into a Map
  * The third that will generate a file with each symptoms and their value sorted by alphabetical order
+ * @author Amina Mounir
+ * @param listSymptoms : a list that store all the symptoms
+ * @param file : a string variable that store the file path
+ * @param counter : a map that store each symptoms with their values
  * 
  */
 public class AnalyticsCounter {
